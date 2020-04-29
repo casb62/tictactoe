fcb724552049d06dc062864e2a5f8dbbd17b0b5f
casb62 <casb62@gmail.com>
2020-04-29 00:04:10 -0300

    Implementado até o item 10.1.2 do livro

--- src/mygame/TictactoeController.java
+++ src/mygame/TictactoeController.java
@@ -5,40 +5,30 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class TictactoeController implements Initializable {
-    
+
+    private boolean isFirstPlayer = true;
+
     @FXML
-    private Label label;
-    
+    private Button clickedButton;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
-}
-
-public class Controller {
-   private boolean isFirstPlayer = true;
-
-   public void buttonClickHandler(ActionEvent evt){
-
-        Button clickedButton = (Button) evt.getTarget();
+        Button clickedButton = (Button) event.getTarget();
         String buttonLabel = clickedButton.getText();
-
-        if ("".equals(buttonLabel) && isFirstPlayer){
+        if ("".equals(buttonLabel) && isFirstPlayer) {
             clickedButton.setText("X");
             isFirstPlayer = false;
-        } else if("".equals(buttonLabel) && !isFirstPlayer){
+        } else if ("".equals(buttonLabel) && !isFirstPlayer) {
             clickedButton.setText("O");
             isFirstPlayer = true;
         }
-   }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
 }
